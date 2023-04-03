b70d1cf134dbbd9a41baa7b642e211cd9bf37376
MahatokyH <69034705+MahatokyH@users.noreply.github.com>
2023-04-03 11:51:30 +0300

    update TDD

--- src/Controller/music/music.controller.spec.ts
+++ src/Controller/music/music.controller.spec.ts
@@ -1,33 +1,87 @@
 /* eslint-disable prettier/prettier */
 import { Test, TestingModule } from '@nestjs/testing';
-import { Music } from '../../Schemas/music.schema';
+import { Music, MusicSchema } from '../../Schemas/music.schema';
 import { MusicRepository } from '../..//Repositories/music.repository';
 import { MusicDto } from '../../DTO/music.dto';
 import { MusicService } from '../../Services/music.service';
 import { MusicController } from './music.controller';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Connection, Model, connect } from 'mongoose';
+import { getModelToken } from '@nestjs/mongoose';
 // THE Injection Dependance are not okay for this test
 describe('MusicController', () => {
   let controller: MusicController;
   let musicService: MusicService;
+  let mongod: MongoMemoryServer;
+  let mongoConnection: Connection;
+  let musiceModel: Model<Music>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    mongoConnection = (await connect(uri)).connection;
+    musiceModel = mongoConnection.model(Music.name, MusicSchema);
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MusicController],
-      providers: [MusicService, MusicRepository],
+      providers: [
+        MusicService,
+        MusicRepository,
+        { provide: getModelToken(Music.name), useValue: musiceModel },
+      ],
     }).compile();
     controller = module.get<MusicController>(MusicController);
     musicService = module.get<MusicService>(MusicService);
   });
 
-  describe('get one', () => {
-    it('should return on music', async () => {
+  afterAll(async () => {
+    await mongoConnection.dropDatabase();
+    await mongoConnection.close();
+    await mongod.stop();
+  });
+
+  // afterEach(async () => {
+  //   const collections = mongoConnection.collections;
+  //   for (const key in collections) {
+  //     const collection = collections[key];
+  //     // console.log(collection);
+  //     await collection.deleteMany({});
+  //   }
+  // });
+
+  describe('create a music', () => {
+    //jest.setTimeout(300000);
+    it('should return a music add', async () => {
+      const music = new Music();
+      music.dateOut = new Date(Date.now());
+      music.singer = ['Singer test'];
+      music.title = 'Title test';
       const result = new Promise<Music>((resolve, rejects) => {
-        resolve(new Music());
+        resolve(music);
       });
-      jest
-        .spyOn(musicService, 'getMusicById')
-        .mockImplementation((musicId: 'XXXX-XXXX-XXXX') => result);
-      expect(await controller.getMusic('XXXX-XXXX-XXXX')).toBe(result);
+      const dto = new MusicDto();
+      dto.dateOut = new Date(Date.now());
+      dto.singer = ['Singer test'];
+      dto.title = 'Title test';
+      // jest
+      //   .spyOn(musicService, 'createMusic')
+      //   .mockImplementation((dto) => result);
+      expect((await controller.createMusic(dto)).singer).toStrictEqual(
+        (await result).singer,
+      );
+    });
+  });
+
+  describe('get one', () => {
+    //jest.setTimeout(300000);
+    it('should return on music', async () => {
+      // const result = new Promise<Music>((resolve, rejects) => {
+      //   resolve(new Music());
+      // });
+      // jest
+      //   .spyOn(musicService, 'getMusicById')
+      //   .mockImplementation((musicId: 'XXXX-XXXX-XXXX') => result);
+      expect(await controller.getMusic('XXXX-XXXX-XXXX')).toBe(null);
     });
   });
 
@@ -36,34 +90,21 @@ describe('MusicController', () => {
       const result = new Promise<Music[]>((resolve, rejects) => {
         resolve([new Music(), new Music()]);
       });
-      jest.spyOn(musicService, 'getMusics').mockImplementation(() => result);
-      expect(await controller.getMusics()).toBe(result);
-    });
-  });
-
-  describe('create a music', () => {
-    it('should return a music add', async () => {
-      const result = new Promise<Music>((resolve, rejects) => {
-        resolve(new Music());
-      });
-      const dto = new MusicDto();
-      jest
-        .spyOn(musicService, 'createMusic')
-        .mockImplementation((dto) => result);
-      expect(await controller.createMusic(dto)).toBe(result);
+      // jest.spyOn(musicService, 'getMusics').mockImplementation(() => result);
+      expect((await controller.getMusics()).length).toBe(1);
     });
   });
 
   describe('update a music', () => {
     it('should return a music update', async () => {
-      const result = new Promise<Music>((resolve, rejects) => {
-        resolve(new Music());
-      });
+      // const result = new Promise<Music>((resolve, rejects) => {
+      //   resolve(new Music());
+      // });
       const dto = new MusicDto();
-      jest
-        .spyOn(musicService, 'updateMusic')
-        .mockImplementation((musicId: 'XXXX-XXXX-XXXX', dto) => result);
-      expect(await controller.updateMusic('XXXX-XXXX-XXXX', dto)).toBe(result);
+      // jest
+      //   .spyOn(musicService, 'updateMusic')
+      //   .mockImplementation((musicId: 'XXXX-XXXX-XXXX', dto) => result);
+      expect(await controller.updateMusic('XXXX-XXXX-XXXX', dto)).toBe(null);
     });
   });
 });
