6789d84d5edcc8b6a321fbbbc014063cfa694b67
MahatokyH <69034705+MahatokyH@users.noreply.github.com>
2023-04-03 14:24:23 +0300

    Update app.module.ts
    use connection in environnement

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,11 @@ import { AppController } from './Controller/app.controller';
 import { AppService } from './app.service';
 import { MusicModule } from './Modules/music.module';
 
+let stringConnection = 'mongodb://localhost:27017/Musica';
+if ('MONGODB_CONNECTION' in process.env)
+  stringConnection = process.env['MONGODB_CONNECTION'];
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/Musica'),
-    MusicModule,
-  ],
+  imports: [MongooseModule.forRoot(stringConnection), MusicModule],
   controllers: [AppController],
   providers: [AppService],
 })
