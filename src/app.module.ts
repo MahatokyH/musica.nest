4777909788b68cc1584298b1fbec1263efa434d6
MahatokyH <69034705+MahatokyH@users.noreply.github.com>
2023-04-06 09:32:28 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './Controller/app.controller';
 import { AppService } from './app.service';
 import { MusicModule } from './Modules/music.module';
 
-let stringConnection = 'mongodb://localhost:27017/Musica';
+let stringConnection = 'mongodb://localhost:27017';
+let databaseName = 'Musica';
 if ('MONGODB_CONNECTION' in process.env)
   stringConnection = process.env['MONGODB_CONNECTION'];
+if ('DATABASE_NAME' in process.env) databaseName = process.env['DATABASE_NAME'];
 @Module({
-  imports: [MongooseModule.forRoot(stringConnection), MusicModule],
+  imports: [
+    MongooseModule.forRoot(stringConnection, { dbName: databaseName }),
+    MusicModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
